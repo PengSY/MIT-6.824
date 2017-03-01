d446857992656c06bd022fa567c0b8487481912e
PengSY <244875002@qq.com>
2017-03-01 22:57:59 +0800

    modified schedule.go, split tasks over workers

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -32,5 +35,75 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+
+	//set DoTaskArgs
+	var args DoTaskArgs
+	args.JobName = jobName
+	args.NumOtherPhase = n_other
+	args.Phase = phase
+
+	//undo tasks and this version don't have a doing task list
+	undoList := make([]int, ntasks)
+	for i := 0; i < ntasks; i = i + 1 {
+		undoList[i] = i
+	}
+	//workers and tasks it is doing
+	wtmap := make(map[string]int)
+	//workers return its rpc address through channel if success
+	rschan := make(chan string)
+	//idle worker list
+	idleWorker := make([]string, 0)
+	//signal there is idleWorker through channel
+	//worker num
+	totalWorker := 0
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		for {
+			select {
+			//if there is a worker ask to register
+			case workerAddr := <-registerChan:
+				idleWorker = append(idleWorker, workerAddr)
+				totalWorker = totalWorker + 1
+				fmt.Println("registerChan")
+			//if a worker finished its task
+			case workerAddr := <-rschan:
+				idleWorker = append(idleWorker, workerAddr)
+				delete(wtmap, workerAddr)
+				fmt.Println("rschan")
+			default:
+				if len(undoList) == 0 {
+					if len(idleWorker) == totalWorker {
+						fmt.Println("all worker idle")
+						defer wg.Done()
+						return
+					}
+				} else if len(idleWorker) > 0 {
+					for len(idleWorker) > 0 {
+						if len(undoList) == 0 {
+							break
+						}
+						args.TaskNumber = undoList[0]
+						undoList = undoList[1:]
+						workerAddr := idleWorker[0]
+						idleWorker = idleWorker[1:]
+						wtmap[workerAddr] = args.TaskNumber
+						if phase == mapPhase {
+							args.File = mapFiles[args.TaskNumber]
+						}
+						go func(srv string, rpcname string, args interface{}, reply interface{}) {
+							call(srv, rpcname, args, reply)
+							fmt.Println("after call")
+							rschan <- srv
+						}(workerAddr, "Worker.DoTask", args, nil)
+					}
+				}
+			}
+		}
+
+	}()
+
+	wg.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
